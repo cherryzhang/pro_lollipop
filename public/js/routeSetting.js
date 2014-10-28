9894a30b8fb342c7d24d3babbba6393ca594f2d3
terryyou <terryyou@cn.ibm.com>
2014-10-28 16:32:50 +0800

    Add auto complete

--- public/js/routeSetting.js
+++ public/js/routeSetting.js
@@ -59,4 +59,29 @@ function getSearchCondition() {
     return obj;
 }
 
+function setAutoComplete(input, panel) {
+    var ac = new BMap.Autocomplete(    //建立一个自动完成的对象
+        {"input" : input
+        ,"location" : map
+    });
+    
+    ac.addEventListener("onhighlight", function(e) {  //鼠标放在下拉列表上的事件
+    var str = "";
+        var _value = e.fromitem.value;
+        var value = "";
+        if (e.fromitem.index > -1) {
+            value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+        }    
+        str = "FromItem<br />index = " + e.fromitem.index + "<br />value = " + value;
+        
+        value = "";
+        if (e.toitem.index > -1) {
+            _value = e.toitem.value;
+            value = _value.province +  _value.city +  _value.district +  _value.street +  _value.business;
+        }    
+        str += "<br />ToItem<br />index = " + e.toitem.index + "<br />value = " + value;
+        $(panel).innerHTML = str;
+    });
+}
+
   
