7ec5fd119be86a8394e95b56cc04fc4227d27560
cherry zhang <cherryzhan@gmail.com>
2014-10-28 16:01:30 +0800

    change on setting page

--- public/js/routeSetting.js
+++ public/js/routeSetting.js
@@ -27,11 +27,36 @@ function enableAddMidPoint(element) {
 }
 
 function addMidPoint(){
-    $('<div class="form-group"><input type="text" placeholder="中途点" ></input></div>').appendTo("#midPointContainer")
+    $('<div class="form-group"><input type="text" placeholder="中途点" ></input><div  class="delete_16"></div></div>').appendTo("#midPointContainer")
+}
+
+function removeMidPoint(e){
+    if(e.target.className == "delete_16")
+        $(e.target).parent().remove();
 }
 
 function getSearchCondition() {
+    var obj = {};
     
+    obj.start = $("#start").val();
+    obj.end = $("#end").val();
+    
+    if($("#midStop")[0].checked){
+        var midPoints = $("#midPointContainer").children();
+        if(midPoints.length>0){
+            obj.mid = [];
+            
+            for(var i = 0; i<midPoints.length; i++) {
+                obj.mid.push($(midPoints).children()[0].value)
+            }
+            
+        }
+    }
+    
+    obj.excludeHighWay = $("#excludeHighway")[0].checked;
+    
+    console.log(obj)
+    return obj;
 }
 
   
