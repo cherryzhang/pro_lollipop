ca14c12653ec7af5be609ccac78ffcb09d40593c
cherry zhang <cherryzhan@gmail.com>
2014-10-29 12:20:35 +0800

    add db connection to save route and get route

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,4 @@
-exports.init = function(app, db) {
+﻿exports.init = function(app, db) {
 
     app.get('/', function(req, res){
         res.render('map');
@@ -15,15 +15,39 @@ exports.init = function(app, db) {
     
     app.get('/user', function(req, res){
         var collection = db.get('usercollection');
-        collection.find({},{},function(e,docs){ //ȡ�����еļ�������, ��Ⱦ��ҳ����,�ؼ�����userlist
+        collection.find({},{},function(e,docs){
             res.json(docs);
         });
-    
     });
-    
+
     app.get('/create', function(req, res){
         var collection = db.get('usercollection');
-        collection.insert({"name":new Date().toLocaleTimeString()})
+        collection.insert({"name":new Date().toLocaleTimeString()});
+    })
+
+    
+    app.get('/getRouteInfo', function(req, res){
+        var collection = db.get('routeInfo');
+        
+        collection.findOne({name:"lastRoute"}).on('success', function(doc){
+            res.json(doc);
+        })
+        
+
+    });
+    
+    app.get('/saveRouteInfo', function(req, res){
+        var collection = db.get('routeInfo');
+        
+        var promise = collection.findOne({name:"lastRoute"});
+        promise.on('success', function (doc) {
+            if(doc) {
+                collection.update(doc._id, { $set: {  start:req.query.start, end:req.query.end}});
+            }
+            else {
+                collection.insert({name:"lastRoute", start:req.query.start, end:req.query.end})
+            }
+        });
         
     
     })
