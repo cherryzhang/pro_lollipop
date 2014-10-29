97e35786c6410ac22ea5d2b12ae8f07161e160fa
cherry zhang <cherryzhan@gmail.com>
2014-10-29 10:07:52 +0800

    add "endrouting"

--- public/js/routeSearch.js
+++ public/js/routeSearch.js
@@ -1,6 +1,18 @@
 
 function searchRoute() {
-    getSearchCondition();
+    var condition = getSearchCondition();
+    
+    searchForDriving(map, condition.start, condition.end);
     
     closeSetting();
+    
+    $("#location").hide();
+    $("#endRouting").show();
+}
+
+function endCurrentRouting() {
+    map.clearOverlays();
+    
+    $("#location").show();
+    $("#endRouting").hide();
 }
